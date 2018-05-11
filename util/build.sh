7b36867a17c4fae79fff00c067b60eb0a37076c6
Tobi Adeyinka <adeyinka.oluwatobi@yahoo.com>
2018-05-11 23:29:11 +0200

    changes to enable YouCompleteMe semaic support in vim

--- util/build.sh
+++ util/build.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 
-cmake -DCMAKE_PREFIX_PATH=lib/wxWidgets/build-debug/ . && make
+cmake -DCMAKE_PREFIX_PATH=lib/wxWidgets/build-debug/ -DCMAKE_EXPORT_COMPILE_COMMANDS=O -DCMAKE_EXPORT_COMPILE_COMMANDS=ON . && make
